6fcc96a082eed01ade47d6e9700b09f73f4863f2
Christoph Häckel <christoph.haeckel@gedankentanken.com>
2020-04-16 12:12:36 +0200

    Timer example with hooks

--- src/webparts/helloWorld/components/HelloWorld.tsx
+++ src/webparts/helloWorld/components/HelloWorld.tsx
@@ -2,21 +2,34 @@ import * as React from "react";
 import styles from "./HelloWorld.module.scss";
 import { IHelloWorldProps } from "./IHelloWorldProps";
 import { escape } from "@microsoft/sp-lodash-subset";
+import { useState, useEffect } from "react";
 
 export default function HelloWorld(
   props: IHelloWorldProps
 ): React.ReactElement<IHelloWorldProps> {
-  let [hello, sayHello] = React.useState("");
-  React.useEffect(() => {
-    sayHello("hook");
-  });
+  const [count, setCount] = useState(0);
+  const tick = () => {
+    setCount((prevCount) => {
+      return prevCount + 1;
+    });
+  };
+
+  useEffect(() => {
+    const interval = setInterval(tick, 1e3);
+    console.log(interval);
+    return () => {
+      console.log("clearInterval");
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <div className={styles.helloWorld}>
       <div className={styles.container}>
         <div className={styles.row}>
           <div className={styles.column}>
             <span className={styles.title}>Welcome to SharePoint!</span>
-            <p className={styles.subTitle}>Hello {hello}</p>
+            <p className={styles.subTitle}>{count}x</p>
             <p className={styles.description}>{escape(props.description)}</p>
             <a href="https://aka.ms/spfx" className={styles.button}>
               <span className={styles.label}>Learn more</span>
